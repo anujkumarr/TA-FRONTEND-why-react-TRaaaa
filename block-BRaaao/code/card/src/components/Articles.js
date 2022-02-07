b8ba012fdf405d66d0e20a6247bf89763f5201e5
akk26 <anujbph@gmail.com>
2022-02-07 21:44:21 +0530

    Refactor

---
+++ block-BRaaao/code/card/src/components/Articles.js
@@ -0,0 +1,27 @@
+import data from '../data';
+
+function Articles() {
+  return (
+    <>
+      <div className="container wrapper flex flex-stretch">
+        {data.map((text) => (
+          <Card key={text.author} info={text} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+function Card(props) {
+  let { urlToImage, title } = props.info;
+  return (
+    <>
+      <div className="card flex-20">
+        <img className="image-width" src={urlToImage} alt="k" />
+        <div className="title">{title}</div>
+      </div>
+    </>
+  );
+}
+
+export default Articles;
